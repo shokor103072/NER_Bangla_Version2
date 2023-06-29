20eb7ae57b76905b5559ed58e78e7079e864ea3c
Shokor A Rahman <47053139+shokor103072@users.noreply.github.com>
2023-06-30 03:46:18 +0600

    Create test.py

---
+++ test.py
@@ -0,0 +1,26 @@
+import spacy
+from spacy.lang.bn import Bengali
+
+# Create a blank Bengali model
+nlp = Bengali()
+
+# Add the transformer component
+nlp.add_pipe("transformer")
+
+# Load English tokenizer, tagger, parser and NER
+nlp = spacy.load("/models_multilingual_bert/model-best")
+
+text_list = [
+    "আব্দুর রহিম নামের কাস্টমারকে একশ টাকা বাকি দিলাম",
+    "১০০ টাকা জমা দিয়েছেন কবির",
+    "ডিপিডিসির স্পেশাল টাস্কফোর্সের প্রধান মুনীর চৌধুরী জানান",
+    "অগ্রণী ব্যাংকের জ্যেষ্ঠ কর্মকর্তা পদে নিয়োগ পরীক্ষার প্রশ্নপত্র ফাঁসের অভিযোগ উঠেছে।",
+    "সে আজকে ঢাকা যাবে",
+]
+for text in text_list:
+    doc = nlp(text)
+
+    print(f"Input: {text}")
+    for entity in doc.ents:
+        print(f"Entity: {entity.text}, Label: {entity.label_}")
+    print("---")
